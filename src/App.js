52a5a599f2cc56ebe59377c0a26848541ed2cede
Asotikov Anton <asant@yandex.ru>
2020-04-18 17:10:57 +0300

    add task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import List from './components/List';
+import Details from './components/Details';
 
 function App() {
+  const url = process.env.REACT_APP_URL;
+  const [list, setList] = useState([]);
+  const [details, setDetails] = useState({});
+  const [currentFetch, setCurrentFetch] = useState({
+    state: setList,
+    itemUrl: `${url}users.json`,
+  });
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch(currentFetch.itemUrl);
+        if (!response.ok) {
+          throw new Error(response.statusText);
+        }
+        const data = await response.json();
+        currentFetch.state(data);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setLoading(false);
+      }
+
+    };
+    fetchData();
+  }, [currentFetch]);
+
+  const handleClick = (id) => {
+    if(id !== details.id) {
+      setList(prevList => prevList.map((item) => {
+        let activeItem = false;
+        if (item.id === id) {
+          activeItem = true;
+        }
+        return {
+          id: item.id,
+          name: item.name,
+          active: activeItem,
+        };
+      }))
+
+      setCurrentFetch({
+        state: setDetails,
+        itemUrl: `${url}${id}.json`,
+      });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isLoading && <p className="loading">Loading...</p>}
+      <List data={list} onClickItem={handleClick} />
+      {details.id && <Details data={details} />}
+    </>
   );
 }
 
